9e3293c14e643798b13139ccda8dbde986660350
Bracerr <89707169+Bracerr@users.noreply.github.com>
2025-02-18 23:45:54 +0300

    feat: LAB1-3 add db models

---
+++ backend/src/models/user.js
@@ -0,0 +1,35 @@
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from '../config/db.js';
+
+class User extends Model {}
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
+    },
+    createdAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+}, {
+    sequelize,
+    modelName: 'User',
+    timestamps: false,
+});
+
+
+export default User;
