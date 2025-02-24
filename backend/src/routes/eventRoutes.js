ba505e55d7da01321966864f06d624eecd48da4a
Bracerr <89707169+Bracerr@users.noreply.github.com>
2025-02-24 12:14:06 +0300

    fix: change-error-handling update error middleware (#14)

--- backend/src/routes/eventRoutes.js
+++ backend/src/routes/eventRoutes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { eventHandler } from "../handlers/eventHandler.js";
 import { upload } from "../config/multer.js";
+import { validateIdMiddleware } from "../middleware/middleware.js";
 
 const eventRouter = new Router();
 
 eventRouter.get('/', eventHandler.getAllEvents);
-eventRouter.get('/:id', eventHandler.getEventById);
+eventRouter.get('/:id', validateIdMiddleware, eventHandler.getEventById);
 eventRouter.post('/', eventHandler.createEvent);
-eventRouter.put('/:id', eventHandler.updateEvent);
-eventRouter.delete('/:id', eventHandler.deleteEvent);
-eventRouter.post('/:id/image', upload.single('image'), eventHandler.uploadEventImage);
+eventRouter.put('/:id', validateIdMiddleware, eventHandler.updateEvent);
+eventRouter.delete('/:id', validateIdMiddleware, eventHandler.deleteEvent);
+eventRouter.post('/:id/image', validateIdMiddleware, upload.single('image'), eventHandler.uploadEventImage);
 
 export { eventRouter };
